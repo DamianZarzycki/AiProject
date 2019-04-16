c682d0bb1e2d92993f570c27c064940ff99ba65c
Damian Zarzycki <36889073+DamianZarzycki@users.noreply.github.com>
2019-04-16 14:28:40 +0200

    Update projekcik.py
    Adding detection of object (orange color) and showing center of it by the small dot

--- projekcik.py
+++ projekcik.py
@@ -27,6 +27,19 @@ while (cap.isOpened()):
 
         #checking if there is an object that is located between boundaries of color that we have chosen
         filter = cv2.inRange(hsv_view, low_boundary_orange, high_boundary_orange)
+        
+         contours, _ = cv2.findContours(filter.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        #If the contours where detected then...
+        if len(contours) > 0:
+            cv2.putText("Contours detected!",[47, 255, 255])
+            main_contour = max(contours, key=cv2.contourArea)
+            main_contour_area = cv2.contourArea(main_contour)
+
+            # Here You need to prase the FLOAT into INT because funtion takes integers as an argument but gets float instead what will cause an error
+            if main_contour_area >= min_valid_area:
+                (contour_x, contour_y), contour_radius = cv2.minEnclosingCircle(main_contour)
+                cv2.circle(frame, (int(contour_x), int(contour_y)), int(contour_radius / 20), (0, 255, 0), -1)
 
         #making bitwise AND operation on our rt frame with filter in image variable (mask is some bit operation 8bits array or something)
         res = cv2.bitwise_and(frame,frame,mask=filter)

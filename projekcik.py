5881d9ddfcb8397778cc94d8a33c3177082bb1c2
Damian Zarzycki <36889073+DamianZarzycki@users.noreply.github.com>
2019-05-01 19:07:06 +0200

    Updating
    Fixing not working code, changing detection and improve contours around object

--- projekcik.py
+++ projekcik.py
@@ -1,56 +1,65 @@
 import cv2
 import numpy as np
 
+#using 0 because its a webcamp
 cap = cv2.VideoCapture(0)
 
 if (cap.isOpened() == False):
     print("!!! Error during opening camera !!!")
 else:
     print("!!! Camera has been opened !!!")
-    
-# HSV color range describtion:
-# H stands for color
-# S stands for strenth of color
-# V stands for lighting of the color
 
 low_boundary_orange=np.array([10,150,255])
-high_boundary_orange=np.array([47,255,255])
+high_boundary_orange=np.array([20,255,255])
 
+#creating empty oints array
+array = []
 
 while (cap.isOpened()):
     #our original frame image
     ret, frame = cap.read()
-    
+
+#W SKROCIE JAK DZIAŁA CALA FILTRACJA:
+# zamieniamy rbg na hsv, zabieg który ulatwia nam okreslanie zakresu koloru ktorego poszukujemy (tak ja to zrozumialem)
+# nastepnie, korzystajac z funkcji inRange() 'wrzucamy' naz obraz do 'pudelka' z granicami koloru pomaranczowego
+# kolejnym krokiem jest operacja AND dzieki ktorej wszystko co jest TRUE bedzie miec mozliwosc wyswietlenie w naszym kolorze w naspenych krokach
+    #jedynki nachodza na naszej masce (filter) i pokazuja kolor na ekranie
+
+
     if ret == True:
-         #creating black-white binary image of captured video
+        #converting from rbg/bgr to hsv image of captured video
         hsv_view = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        blurred = cv2.GaussianBlur(hsv_view, (3, 3), 0)
 
-        #checking if there is an object that is located between boundaries of color that we have chosen
+        #checking if there is an object that is located between boundaries of color that we have chosen so for this project its everything thats orange
+        #if its in the range its 1 (white) if not its 0
         filter = cv2.inRange(hsv_view, low_boundary_orange, high_boundary_orange)
-        
-         contours, _ = cv2.findContours(filter.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        #If the contours where detected then...
-        if len(contours) > 0:
-            cv2.putText("Contours detected!",[47, 255, 255])
-            main_contour = max(contours, key=cv2.contourArea)
-            main_contour_area = cv2.contourArea(main_contour)
-
-            # Here You need to prase the FLOAT into INT because funtion takes integers as an argument but gets float instead what will cause an error
-            if main_contour_area >= min_valid_area:
-                (contour_x, contour_y), contour_radius = cv2.minEnclosingCircle(main_contour)
-                cv2.circle(frame, (int(contour_x), int(contour_y)), int(contour_radius / 20), (0, 255, 0), -1)
 
         #making bitwise AND operation on our rt frame with filter in image variable (mask is some bit operation 8bits array or something)
+        #when its one
         res = cv2.bitwise_and(frame,frame,mask=filter)
         #frame = cv2.flip(frame, 1)
 
+        #getting the contours. Contours function returns 3 values and we only need 1
+        contours, _ = cv2.findContours(filter, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+        for contour in contours:
+            area = cv2.contourArea(contour)
+
+            if area > 1000:
+                #on what, what draw, from where we stsart -1 means everything, color, thickness
+                cv2.drawContours(frame, contours, -1 , (0,255,0), 3)
+
+
+
+
+        frame = cv2.flip(frame, 1)
         #our frame with rt video
         cv2.imshow("NAI PROJECT:)", frame)
         cv2.imshow("COLOR FILTER", res)
         cv2.imshow("filter", filter)
 
-        
+
         if cv2.waitKey(25) & 0xFF == ord('x'):
             break
     else:
